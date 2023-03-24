493c7dc1d1c6b21b84ed8431ec3130da35a6745c
Mateus Elias <meap@cin.ufpe.br>
2023-03-24 06:41:40 -0300

    feat: Board class created

---
+++ classes/Board.py
@@ -0,0 +1,23 @@
+import pygame
+from classes.Square import Square
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tile_width = width // 8
+        self.tile_height = height // 8
+        self.squares = self.generate_squares()
+
+    def generate_squares(self):
+        squares = []
+        
+        for x in range(8):
+            for y in range(8):
+                squares.append(Square(x,  y, self.tile_width, self.tile_height))
+        
+        return squares
+    
+    def draw(self, display):
+        for square in self.squares:
+            square.draw(display)
