0f70ddb0304e810958bdc09d0902a3b7c357bbbe
Mateus Elias <meap@cin.ufpe.br>
2023-04-01 02:40:10 -0300

    feat(classes): Piece class created

---
+++ classes/Piece.py
@@ -0,0 +1,43 @@
+class Piece:
+    def __init__(self, pos, color, board):
+        self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.color = color
+        self.has_moved = False
+
+    def get_moves(self, board):
+        output = []
+        for direction in self.get_possible_moves(board):
+            for square in direction:
+                if square.occupying_piece is not None:
+                    if square.occupying_piece.color == self.color:
+                        break
+                    else:
+                        output.append(square)
+                        break
+                else:
+                    output.append(square)
+        return output
+    
+    def get_valid_moves(self, board):
+        output = []
+        for square in self.get_moves(board):
+            if not board.is_in_check(self.color, board_change=[self.pos, square.pos]):
+                output.append(square)
+        return output
+    
+    def move(self, board, square, force=False):
+        for i in board.squares:
+            i.highlight = False
+        if square in self.get_valid_moves(board) or force:
+            prev_square = board.get_square_from_pos(self.pos)
+            self.pos, self.x, self.y = square.pos, square.x, square.y
+            prev_square.occupying_piece = None
+            square.occupying_piece = self
+            board.selected_piece = None
+            self.has_moved = True
+            return True
+        else:
+            board.selected_piece = None
+            return False
